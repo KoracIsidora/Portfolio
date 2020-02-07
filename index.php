b7a4412ef886a8cc17ef76a385d5226e1a843dfc
Isidora <korac.isidora@gmail.com>
2020-02-07 15:00:36 +0100

    Animation changes

--- index.php
+++ index.php
@@ -44,6 +44,7 @@ if (filter_has_var(INPUT_POST, 'submit')) {
 
 <head>
     <title>Isidora Korac | Portfolio</title>
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
 </head>
@@ -58,55 +59,55 @@ if (filter_has_var(INPUT_POST, 'submit')) {
                 <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
-            <section id="home">
-                <h1>Welcome!</h1>
-                <p class="lead">My name is Isidora Korac and I am Front End Developer</p>
-            </section>
-            <section id="about">
-                <h1>About Me <i class="far fa-smile-beam"></i></h1>
-                <p class="lead"> My name is Isidora Korac, and I have recently started developing web pages.
-                    I finished <a href='https://itbootcamp.rs/' target='blank_'>IT Bootcamp</a> course, within which I learned Web Development.
-                    If you decide to continue reading my portfolio, you will be able to
-                    see some of my recent projects.
-                    I am trying to learn more every day and to gain new knowledge with every
-                    project I do. I consider myself hard-working, capable and very motivated person.
-                    Always opened for learning and improving myself.
-                    My CV can be found <a href="assets/cv.pdf" target="blank_">here</a>. </p>
-            </section>
-            <section id="projects">
-                <h1>Projects</h1>
-                <p >Some of the projects I did so far:</p>
-                <div class="proj">
-                    <a href="https://koracisidoraportfoli.000webhostapp.com/" target="blank_"><img src="assets/forum.png" alt="forum" /></a>
-                    <a href="http://gamesplatform.000webhostapp.com/" target="blank_"><img src="assets/game.png" alt="Game" /></a>
-                    <p class="lead">All of my work can be found at my <a href="https://github.com/KoracIsidora" target="blank_">Git Hub</a> account.</p>
+        <section id="home" data-aos="fade-up">
+            <h1>Welcome!</h1>
+            <p class="lead">My name is Isidora Korac and I am Front End Developer</p>
+        </section>
+        <section id="about" data-aos="fade-up">
+            <h1>About Me <i class="far fa-smile-beam"></i></h1>
+            <p class="lead"> My name is Isidora Korac, and I have recently started developing web pages.
+                I finished <a href='https://itbootcamp.rs/' target='blank_'>IT Bootcamp</a> course, within which I learned Web Development.
+                If you decide to continue reading my portfolio, you will be able to
+                see some of my recent projects.
+                I am trying to learn more every day and to gain new knowledge with every
+                project I do. I consider myself hard-working, capable and very motivated person.
+                Always opened for learning and improving myself.
+                My CV can be found <a href="assets/cv.pdf" target="blank_">here</a>. </p>
+        </section>
+        <section id="projects" data-aos="fade-up">
+            <h1>Projects</h1>
+            <p>Some of the projects I did so far:</p>
+            <div class="proj">
+                <a href="https://koracisidoraportfoli.000webhostapp.com/" target="blank_"><img src="assets/forum.png" alt="forum" /></a>
+                <a href="http://gamesplatform.000webhostapp.com/" target="blank_"><img src="assets/game.png" alt="Game" /></a>
+                <p class="lead">All of my work can be found at my <a href="https://github.com/KoracIsidora" target="blank_">Git Hub</a> account.</p>
+            </div>
+        </section>
+        <section id="contact" data-aos="fade-up">
+            <h1>Contact</h1>
+            <p class="lead">If you want to contact me, my email is korac.isidora@gmail.com, <br />
+                but you can also send me a message here:
+            </p>
+            <?php if ($msg != '') : ?>
+                <div class="alert <?php echo $msgClass; ?>"><?php echo $msg; ?></div>
+            <?php endif; ?>
+            <form method="post" class="lead" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                <div class="form-group">
+                    <label>Name</label>
+                    <input type="text" name="name" class="form-control" value="<?php echo isset($_POST['name']) ? $name : ''; ?>">
                 </div>
-            </section>
-            <section id="contact">
-                <h1>Contact</h1>
-                <p class="lead">If you want to contact me, my email is korac.isidora@gmail.com, <br />
-                    but you can also send me a message here:
-                </p>
-                <?php if ($msg != '') : ?>
-                    <div class="alert <?php echo $msgClass; ?>"><?php echo $msg; ?></div>
-                <?php endif; ?>
-                <form method="post" class="lead" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                    <div class="form-group">
-                        <label>Name</label>
-                        <input type="text" name="name" class="form-control" value="<?php echo isset($_POST['name']) ? $name : ''; ?>">
-                    </div>
-                    <div class="form-group">
-                        <label>Email</label>
-                        <input type="text" name="email" class="form-control" value="<?php echo isset($_POST['email']) ? $email : ''; ?>">
-                    </div>
-                    <div class="form-group">
-                        <label>Message</label>
-                        <textarea name="message" class="form-control"><?php echo isset($_POST['message']) ? $message : ''; ?></textarea>
-                    </div>
-                    <br>
-                    <button type="submit" name="submit" class="btn">SUBMIT</button>
-                </form>
-            </section>
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="text" name="email" class="form-control" value="<?php echo isset($_POST['email']) ? $email : ''; ?>">
+                </div>
+                <div class="form-group">
+                    <label>Message</label>
+                    <textarea name="message" class="form-control"><?php echo isset($_POST['message']) ? $message : ''; ?></textarea>
+                </div>
+                <br>
+                <button type="submit" name="submit" class="btn">SUBMIT</button>
+            </form>
+        </section>
         <footer class="lead">
             <label><a href="https://www.facebook.com/koraisidora" target="blank_">Facebook</a></label>
             <label><a href="https://www.instagram.com/isidora_kora/" target="blank_">Instagram</a></label>
@@ -114,6 +115,12 @@ if (filter_has_var(INPUT_POST, 'submit')) {
             <label><a href="https://github.com/KoracIsidora" target="blank_">Git Hub</a></label>
             <label>&copy; Isidora Korac 2020</label>
         </footer>
+        <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+        <script>
+            AOS.init({
+                duration: 1000
+            });
+        </script>
         <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@15.0.0/dist/smooth-scroll.polyfills.min.js"></script>
         <script type="text/javascript" src="index.js"></script>
 </body>
